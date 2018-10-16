416ea4f3724f84eb10e076fa9f28cd98084e5a8a
Rodrigo <rodrigofabcas@gmail.com>
2018-10-16 20:40:43 -0300

    multiple images

--- src/pages/cadastro-projeto/cadastro-projeto.ts
+++ src/pages/cadastro-projeto/cadastro-projeto.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FormsModule } from '@angular/forms';
+import { MateriasProvider } from '../../providers/materias/materias';
 
 /**
  * Generated class for the CadastroProjetoPage page.
@@ -19,11 +20,11 @@ export class CadastroProjetoPage {
   titulo: string;
   texto: string;
   colaboradores: string;
-  nomeImagem = "default";
-  image;
+  nomeImagem: string[] = [];
+  image: any[] = [];
   temImagem = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private materias: MateriasProvider) {
   }
 
   ionViewDidLoad() {
@@ -37,18 +38,33 @@ export class CadastroProjetoPage {
   }
   
   readThis(inputValue: any): void {
-    let file: File = inputValue.files[0];
-    var myReader: FileReader = new FileReader();
+    let file: File[] = [];
+    console.log(inputValue);
+    for (let i = 0; i < inputValue.files.length; i++) {
+      file.push(inputValue.files[i]);
+    }
+    console.log(file);
+    
+    
 
-    this.nomeImagem = file.name;
-    // let myReader: FileReader = new FileReader();
+    file.forEach(element => {
+      this.nomeImagem.push(element.name);
+    });
+
+    
+
+    file.forEach(element => {
+      let myReader: FileReader = new FileReader();
+      myReader.readAsDataURL(element);
+      
+      myReader.onloadend = ((e) => {
+        this.image.push(myReader.result); 
+        this.temImagem = true;
+      })
+    });
+    
+  }
+  onSubmit() {
   
-     myReader.onloadend = ((e) => {
-      this.image = myReader.result; 
-      this.temImagem = true;
-     })
-     
-     myReader.readAsDataURL(file);
-     console.log(file);
   }
 }
