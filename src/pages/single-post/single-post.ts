4a03e29c06fcb4e0f843dc1a1ae17f062c884df5
Davi Rodrigues <davisr@id.uff.br>
2018-10-16 18:52:39 -0300

    single-post leva pra perfil

--- src/pages/single-post/single-post.ts
+++ src/pages/single-post/single-post.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { PerfilPage } from '../perfil/perfil';
 
 /**
  * Generated class for the SinglePostPage page.
@@ -20,6 +21,9 @@ export class SinglePostPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SinglePostPage');
+  }
+  entraPerfil(){
+    this.navCtrl.setRoot(PerfilPage);
   }
 
 }
